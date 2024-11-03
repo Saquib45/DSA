9a41c6263a245d115580af6747844097e045d005
Saquib45 <saquibmaniyart@gmail.com>
2024-11-03 20:44:43 +0530

    level order traversal

--- sakib/levelorder.cpp
+++ sakib/levelorder.cpp
@@ -12,38 +12,59 @@ class TreeNode{
         right=NULL;
     }
 };
-TreeNode* input(TreeNode* root){
+TreeNode* input(TreeNode* &root){
     cout<<"Enter the root data:"<<endl;
     int data;
     cin>>data;
     root=new TreeNode(data);
     if(root==NULL) return NULL;
-    queue<TreeNode*> st;
-    st.push(root);
-    while(!st.empty()){
-        TreeNode* temp=st.front();
-        st.pop();
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        TreeNode* temp=q.front();
+        q.pop();
         cout<<"enter the left data of:"<<temp->data<<endl;
         int leftdata;
         cin>>leftdata;
         if(leftdata!=-1){
-            TreeNode* leftnode=new TreeNode(leftdata);
-            temp->left=leftnode;
-            st.push(leftnode);
+            temp->left=new TreeNode(leftdata);
+            q.push(temp->left);
         }
 
         cout<<"enter the right data of:"<<temp->data<<endl;
         int rightdata;
         cin>>rightdata;
         if(rightdata!=-1){
-            TreeNode* rightnode=new TreeNode(rightdata);
-            temp->right=rightnode;
-            st.push(rightnode);
+            temp->right=new TreeNode(rightdata); 
+            q.push(temp->right);
         }
+    }
+    return root;
+}
+
+void iterative_levelordered(TreeNode* root){
+    queue<TreeNode*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        TreeNode* temp=q.front();
+        q.pop();
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()) q.push(NULL);
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+        
     }
 }
 int main(){
     TreeNode* root=NULL;
     root=input(root);
+    cout<<"iterative output:"<<endl;
+    iterative_levelordered(root);
     return 0;
 }
